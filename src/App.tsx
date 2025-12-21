4f7751942d0b4603a5c07762eb9415f65b722f2d
Jorge <jorge.medi.08@gmail.com>
2025-12-21 00:55:53 -0600

    Commit al repositorio

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+import type WeatherForecast from './weatherforecast.model'
+
+function App() {
+
+  const [weatherforecast, setWeatherforecast] = useState<WeatherForecast[]>()
+  
+  useEffect( () =>{
+    fetch(`${import.meta.env.VITE_API_URL}/weatherforecast`)
+    .then(respuesta => respuesta.json()
+    .then(json => setWeatherforecast(json)))
+  }, [])
+  return (
+    <>
+      {weatherforecast ? <div>
+        <ul>
+        {
+          weatherforecast.map(wf => <li key={wf.date}>
+            {`${wf.date} - ${wf.summary} (${wf.temperatureC}C)`}
+          </li>)
+        }
+        </ul>
+      </div> : 'cargando...'}
+    </>
+  )
+}
+
+export default App
